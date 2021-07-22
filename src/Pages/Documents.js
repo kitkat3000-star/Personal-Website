7c92563afec9cb33a88079e9692684509b88df5e
kitkat3000-star <himaja.kakade@gmail.com>
2021-07-22 14:16:50 +0400

    v2

--- src/Pages/Documents.js
+++ src/Pages/Documents.js
@@ -1,15 +1,21 @@
+import React from "react";
 import styled from "styled-components";
 import MotionHoc from "./MotionHoc";
+import { makeStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload'
+import Typography from '@material-ui/core/Typography';
+import Resume from "../assets/Himaja Kakade CV1.pdf";
+
 
 const Document = styled.div`
-    width:100vw;
-    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-color: #FFD1E0;
 
     h1{
         position:absolute;
@@ -20,6 +26,7 @@ const Document = styled.div`
         font-size: 70px;
     }
 
+
     span{
         position: absolute;
         bottom:0;
@@ -29,24 +36,98 @@ const Document = styled.div`
     }
 `;
 
-const File = styled.div`
-    position:absolute;
-    display: flex;
-    top: 150px;
-    left: 700px;
-`;
-
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 280,
+      position: "absolute",
+      top:190,
+      left: 250,
+      boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+    },
+    root1: {
+        maxWidth: 280,
+        position: "absolute",
+        top:190,
+        right: 250,
+        boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+      },
+    media: {
+      height: 130,
+    },
+    button:{
+        left: 90,
+        bottom: 8,
+        width: 90,
+        fontFamily:"'Nanum Pen Script', cursive",
+        fontSize: 19,
+    },
+    heading: {
+    fontFamily:"'Amatic SC', cursive",
+    fontSize: 38
+      },
+    para: {
+    fontFamily:"'Nanum Pen Script', cursive",
+    fontSize: 20
+      },
+ } );
 
 const DocumentsComponent = () => {
+    const classes = useStyles();
     return (
-        // <Document>
-            <h1> My Documents </h1>
-        //     <File>
-        //         <input type="file" class="form-control" multiple></input>
-        //         {/* <Button variant="contained" color="primary" startIcon={<CloudUploadIcon />}>Upload</Button>  */}
-        //     </File>
-        //     <span>©2021 Himaja Kakade. All Rights Reserved.</span>
-        // </Document>
+        <div>
+            <Document>
+                <h1> My Documents </h1>
+                <span>©2021 Himaja Kakade. All Rights Reserved.</span>
+            </Document>
+            
+            <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                        className={classes.media}
+                        image="https://nz.hudson.com/wp-content/cache/bb-plugin/cache/resume-examples-1024x731-landscape.png"
+                        title="My resume"
+                        />
+                        <CardContent>
+                        <Typography className={classes.heading} gutterBottom variant="h5" component="h2" align="center" fontStyle='italic'>
+                         Resume
+                        </Typography>
+                        <Typography className={classes.para} variant="body2" color="textSecondary" component="p" align="center">
+                            My resume for Software/UI-UX developer
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" variant="contained" color="primary" className={classes.button} href={Resume}>
+                            View
+                        </Button>
+                    </CardActions>
+            </Card>
+            <Card className={classes.root1}>
+                    <CardActionArea>
+                        <CardMedia
+                        className={classes.media}
+                        image="https://nz.hudson.com/wp-content/cache/bb-plugin/cache/resume-examples-1024x731-landscape.png"
+                        title="My resume"
+                        />
+                        <CardContent>
+                        <Typography className={classes.heading} gutterBottom variant="h5" component="h2" align="center" fontStyle='italic'>
+                         Document
+                        </Typography>
+                        <Typography className={classes.para} variant="body2" color="textSecondary" component="p" align="center">
+                            Nothing here yet! ..............................
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" variant="contained" color="primary" className={classes.button}>
+                            View
+                        </Button>
+                    </CardActions>
+            </Card>
+            <div>
+                <img src="https://i.pinimg.com/originals/0e/47/72/0e47720e157806d45d45c736fb2fa920.gif" width="100%" height="800px"/>
+            </div>
+        </div>
     )
 }
 
--- src/Pages/Interests.js
+++ src/Pages/Interests.js
@@ -1,10 +1,79 @@
 import MotionHoc from "./MotionHoc";
+import "./Interests.scss"
+import styled from "styled-components";
+
+const Interest = styled.div`
+display:flex;
+width:100vw;
+height:100vh;
+background-color: #dabfde;
+`;
 
 const InterestsComponent = () => {
     return (
-        <h1>
-            Interests
-        </h1>
+        <Interest>
+        <section id="timeline">
+            <div className="tl-item">
+                
+                <div className="tl-bg"><img src="https://lh6.ggpht.com/HlgucZ0ylJAfZgusynnUwxNIgIp5htNhShF559x3dRXiuy_UdP3UQVLYW6c=s1200"/></div>
+                
+                <div className="tl-year">
+                <p className="f2 heading--sanSerif">Art</p>
+                </div>
+
+                <div className="tl-content">
+                <h1>Doodling Away</h1>
+                <p>I thoroughly enjoy making doodles. It really brings out my creative side. I also occassionally enjoy sketching and painting.</p>
+                </div>
+
+            </div>
+
+            <div class="tl-item">
+                
+                <div class="tl-bg"><img src="https://img3.goodfon.com/wallpaper/nbig/a/ba/smoke-dancer-dym-devushka-tanec.jpg" width="700px" height="700px"/></div>
+                
+                <div className="tl-year">
+                <p class="f2 heading--sanSerif">Dance</p>
+                </div>
+
+                <div class="tl-content">
+                <h1 class="f3 text--accent ttu">Like no one's Watching</h1>
+                <p>If theres music, there needs to be a tap. I have enjoyed dancing as a child and even more so now. I forget my worries in the rhythm of the music. I have performed at some school and university events too and they are some of my best memories.</p>
+                </div>
+
+            </div>
+
+            <div class="tl-item">
+                
+                <div class="tl-bg"><img src="https://i.pinimg.com/736x/6d/4b/80/6d4b809a6e1fb3745e7d76caee55a55c.jpg" height="850px"/></div>
+                
+                <div class="tl-year">
+                <p class="f2 heading--sanSerif">Formula1</p>
+                </div>
+
+                <div class="tl-content">
+                <h1 class="f3 text--accent ttu">Drive to Survive</h1>
+                <p>Perseverance. Diligence. Determination. <br/>If there is an example of these words it perhaps has to be this. The tension is real. But thats what makes it the real deal.</p>
+                </div>
+
+            </div>
+
+            <div class="tl-item">
+                
+                <div class="tl-bg"><img src="https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/></div>
+                
+                <div class="tl-year">
+                <p class="f2 heading--sanSerif">Film Fanatic?</p>
+                </div>
+
+                <div class="tl-content">
+                <h1 class="f3 text--accent ttu">Here's Johnny!</h1>
+                <p>If the pandemic has taught me something its that movies and tv shows are a man's best friend. I.E. after dogs and books ofcourse. I enjoy watching well made cinema, it definitely is like the ice cream on top feeling. </p>
+                </div>
+
+            </div>
+        </section>
+</Interest>
     )
 }
 const Interests = MotionHoc(InterestsComponent);
